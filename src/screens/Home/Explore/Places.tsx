7a5e80436e64eb3fbcace08fe9f8e9f7ee2283ac
alekseytsvetkov <code.aleksey@icloud.com>
2021-06-08 02:22:10 +0300

    added FlatList to Places

--- src/screens/Home/Explore/Places.tsx
+++ src/screens/Home/Explore/Places.tsx
@@ -1,8 +1,40 @@
 import { observer } from 'mobx-react';
 import React, { useState } from 'react';
-import { ScrollView, View, Text, RefreshControl } from 'react-native';
+import { ScrollView, View, Text, RefreshControl, FlatList, StyleSheet, StatusBar } from 'react-native';
 import { wait } from '_app/utils';
-import { s } from './styles';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+  {
+    id: '4',
+    title: 'First Item',
+  },
+  {
+    id: '5',
+    title: 'Second Item',
+  },
+  {
+    id: '6',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const Places = () => {
   const [refreshing, setRefreshing] = useState(false);
@@ -12,13 +44,35 @@ const Places = () => {
     wait(2000).then(() => setRefreshing(false));
   }, []);
 
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
   return (
     <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-      <View style={s.container}>
-        <Text>Places!</Text>
-      </View>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export const PlacesScreen = observer(Places);
