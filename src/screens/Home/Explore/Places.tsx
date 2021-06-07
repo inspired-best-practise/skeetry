2d939f9c370ed2fa9126fedb3d43d472b4dce0f8
alekseytsvetkov <code.aleksey@icloud.com>
2021-06-08 02:04:04 +0300

    added RefreshControl

--- src/screens/Home/Explore/Places.tsx
+++ src/screens/Home/Explore/Places.tsx
@@ -1,11 +1,19 @@
 import { observer } from 'mobx-react';
-import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, RefreshControl } from 'react-native';
+import { wait } from '_app/utils';
 import { s } from './styles';
 
 const Places = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
-    <ScrollView>
+    <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
       <View style={s.container}>
         <Text>Places!</Text>
       </View>
--- src/screens/Home/Explore/States.tsx
+++ src/screens/Home/Explore/States.tsx
@@ -1,11 +1,19 @@
 import { observer } from 'mobx-react';
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, RefreshControl } from 'react-native';
+import { wait } from '_app/utils';
 import { s } from './styles';
 
 const States = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
-    <ScrollView>
+    <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
       <View style={s.container}>
         <Text>States!</Text>
       </View>
