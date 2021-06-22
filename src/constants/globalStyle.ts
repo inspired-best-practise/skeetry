1ffcf27f42c683a49ee73443bc419c502b78748a
alekseytsvetkov <code.aleksey@icloud.com>
2021-06-22 21:24:16 +0300

    updated map

--- src/constants/globalStyle.ts
+++ src/constants/globalStyle.ts
@@ -1,7 +1,8 @@
 import { TextStyle } from 'react-native';
+import { MapStyleElement } from 'react-native-maps';
 
 export const radius = {
-  s: 5,
+  s: 6,
   m: 8,
   l: 16,
 };
@@ -113,3 +114,166 @@ export const xsmallBold: TextStyle = {
   ...xsmall,
   fontWeight: '700',
 };
+
+export const mapGfxStyle: MapStyleElement[] = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'on',
+        color: '#000',
+        invert_lightness: true,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#bdbdbd',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#eeeeee',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e5e5e5',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dadada',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e5e5e5',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#eeeeee',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#c9c9c9',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+];
--- src/screens/Home/Explore/CardScreen/CardScreen.tsx
+++ src/screens/Home/Explore/CardScreen/CardScreen.tsx
@@ -4,8 +4,8 @@ import { View, Text, Image, TouchableHighlight, ScrollView, Pressable } from 're
 import { SharedElement } from 'react-navigation-shared-element';
 import { SCREEN_WIDTH } from '_app/utils/getDimensions';
 import { s } from './styles';
-import * as Icon from 'react-native-heroicons/solid';
-// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { mapGfxStyle, radius } from '_app/constants';
 
 const CardDetailScreen = ({ route, navigation }) => {
   const { item } = route.params;
@@ -13,7 +13,7 @@ const CardDetailScreen = ({ route, navigation }) => {
   return (
     <View style={s.container}>
       <SharedElement id={`item.${item.id}.image`}>
-        <Image style={{ width: SCREEN_WIDTH, height: 300 }} source={{ uri: item.images[0].src }} resizeMode="cover" />
+        <Image style={{ width: SCREEN_WIDTH, height: 300 }} source={{ uri: item.images[2].src }} resizeMode="cover" />
       </SharedElement>
       <ScrollView style={s.content}>
         <View style={s.section}>
@@ -36,20 +36,33 @@ const CardDetailScreen = ({ route, navigation }) => {
         </View>
         <View style={s.section}>
           <Text style={s.sectionTitle}>Where you'll be</Text>
-          <Text>Google map</Text>
-          {/* <MapView
-            style={{
-              flex: 1,
-              width: '100%',
-            }}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          /> */}
+          <Pressable onPress={() => navigation.navigate('AddChooser')}>
+            <MapView
+              style={{
+                flex: 1,
+                width: '100%',
+                height: 200,
+                borderRadius: radius.s,
+                marginTop: 14,
+              }}
+              pitchEnabled={false}
+              scrollEnabled={false}
+              zoomControlEnabled={false}
+              zoomEnabled={false}
+              rotateEnabled={false}
+              provider={PROVIDER_GOOGLE}
+              mapType="standard"
+              moveOnMarkerPress={false}
+              pointerEvents="none"
+              customMapStyle={mapGfxStyle}
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.09,
+                longitudeDelta: 0.04,
+              }}
+            />
+          </Pressable>
         </View>
 
         <View style={s.section}>
