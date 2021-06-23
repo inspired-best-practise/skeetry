7472cd07144157a764a444879deddcf47f4b5afd
alekseytsvetkov <code.aleksey@icloud.com>
2021-06-23 03:04:27 +0300

    try bottom sheet

--- src/screens/Home/HomeIndex/HomeIndex.tsx
+++ src/screens/Home/HomeIndex/HomeIndex.tsx
@@ -1,14 +1,59 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 import { View, Text } from 'react-native';
-import { s } from '_app/components/LoadingOverlay/styles';
-import { h4 } from '_app/constants';
+import BottomSheet from 'reanimated-bottom-sheet';
 
 const HomeIndex = () => {
-  return (
-    <View style={s.container}>
-      <Text style={h4}>Home!</Text>
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'white',
+        padding: 16,
+        height: 1000,
+      }}
+    >
+      <Text>Bottom sheet</Text>
     </View>
+  );
+  const renderHeader = () => (
+    <View
+      style={{
+        backgroundColor: 'red',
+        padding: 16,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+      }}
+    >
+      <Text>Header</Text>
+    </View>
+  );
+
+  const sheetRef = React.useRef(null);
+
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'lightblue',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text>Test</Text>
+      </View>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[770, 400]}
+        enabledContentTapInteraction={true}
+        enabledBottomInitialAnimation={true}
+        renderContent={renderContent}
+        overdragResistanceFactor={0} // ?
+        enabledBottomClamp={true}
+        borderRadius={10}
+        // renderHeader={renderHeader}
+      />
+    </>
   );
 };
 
