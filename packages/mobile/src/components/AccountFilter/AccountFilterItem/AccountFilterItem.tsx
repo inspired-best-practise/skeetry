2ab0bfec85eaeba957adb72610f9b0023609eea2
alekseytsvetkov <code.aleksey@icloud.com>
2021-09-10 23:48:18 +0300

    fix(mobile): fix account filter item style

--- packages/mobile/src/components/AccountFilter/AccountFilterItem/AccountFilterItem.tsx
+++ packages/mobile/src/components/AccountFilter/AccountFilterItem/AccountFilterItem.tsx
@@ -9,11 +9,11 @@ import { s } from './styles';
 export const AccountFilterItem = ({ selected, name, title, icon }: TListFilterItemProps) => {
   const setSelected = profileStore(state => state.setSelected);
 
-  // TODO: fix onPress and long press color
   return (
     <TouchableHighlight
       onPress={() => setSelected(name)}
       style={[s.filterItem, selected === name && { backgroundColor: colors.gray900 }]}
+      underlayColor={selected === name ? colors.gray900 : colors.gray100}
     >
       {icon ? icon : <Text style={[s.filterItemTitle, selected === name && { color: colors.gray50 }]}>{title}</Text>}
     </TouchableHighlight>
