1c0bc5b4226a500fbf1e07caf53a291d55e60eb8
alekseytsvetkov <code.aleksey@icloud.com>
2021-09-15 17:35:57 +0300

    refactor(mobile): hide keyboard on press another area

--- packages/mobile/src/screens/Auth/Login/Login.tsx
+++ packages/mobile/src/screens/Auth/Login/Login.tsx
@@ -1,6 +1,14 @@
 import { ApolloError } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 import { tLogo } from '_app/constants';
@@ -50,58 +58,62 @@ export const LoginScreen = () => {
 
   return (
     <SafeAreaView style={s.container}>
-      <KeyboardAvoidingView behavior="position">
-        <View style={s.centerContainer}>
-          <Text style={[tLogo, { marginBottom: 30 }]}>Skeetry</Text>
-          <View style={s.loginForm}>
-            <View style={s.textInputWrapper}>
-              <TextInput
-                autoCapitalize="none"
-                placeholder="Username"
-                onChangeText={onChangeUsername}
-                style={s.input}
-                spellCheck={false}
-              />
-            </View>
-            <View style={s.textInputWrapper}>
-              <TextInput
-                secureTextEntry
-                autoCapitalize="none"
-                placeholder="Password"
-                onChangeText={onChangePasword}
-                style={s.input}
-                spellCheck={false}
-              />
-            </View>
-            <TouchableOpacity
-              onPress={() => navigation.push('ForgotPassword')}
-              style={s.forgotPassword}
-              activeOpacity={1}
-            >
-              <Text style={s.forgotPasswordText}>Forgot your password?</Text>
-            </TouchableOpacity>
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={-20}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View style={s.centerContainer}>
+            <Text style={[tLogo, { marginBottom: 30 }]}>Skeetry</Text>
+            <View style={s.loginForm}>
+              <View style={s.textInputWrapper}>
+                <TextInput
+                  autoCapitalize="none"
+                  placeholder="Username"
+                  onChangeText={onChangeUsername}
+                  style={s.input}
+                  spellCheck={false}
+                />
+              </View>
+              <View style={s.textInputWrapper}>
+                <TextInput
+                  secureTextEntry
+                  autoCapitalize="none"
+                  placeholder="Password"
+                  onChangeText={onChangePasword}
+                  style={s.input}
+                  spellCheck={false}
+                />
+              </View>
+              <TouchableOpacity
+                onPress={() => navigation.push('ForgotPassword')}
+                style={s.forgotPassword}
+                activeOpacity={1}
+              >
+                <Text style={s.forgotPasswordText}>Forgot your password?</Text>
+              </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => {
-                login();
-              }}
-              disabled={!allowLogin && !loading}
-              activeOpacity={0.6}
-              // eslint-disable-next-line react-native/no-inline-styles
-              style={{
-                ...s.btnLogin,
-                opacity: allowLogin ? 1 : 0.6,
-              }}
-            >
-              <Text style={s.btnLoginText}>{!loading ? 'Login' : 'Loading...'}</Text>
-            </TouchableOpacity>
-            {errorLogin && <Text style={s.errorLogin}>{errorLogin.message}</Text>}
+              <TouchableOpacity
+                onPress={() => {
+                  login();
+                }}
+                disabled={!allowLogin && !loading}
+                activeOpacity={0.6}
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  ...s.btnLogin,
+                  opacity: 1,
+                }}
+              >
+                <Text style={s.btnLoginText}>{!loading ? 'Login' : 'Loading...'}</Text>
+              </TouchableOpacity>
+              <Text style={[s.errorLogin, errorLogin ? { opacity: 1 } : { opacity: 0 }]}>
+                {errorLogin && errorLogin.message}
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
       <TouchableOpacity onPress={() => navigation.push('Phone')} activeOpacity={1} style={s.registerWrapper}>
         <Text style={s.registerWrapperText}>
-          <Text style={s.registerWrapperTextBold}>Don't have account?</Text> Register now.
+          <Text style={s.registerWrapperTextBold}>Don't have an account?</Text> Register now.
         </Text>
       </TouchableOpacity>
     </SafeAreaView>
