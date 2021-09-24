313dca16f10f9645b65eb93e9e77738c6aa59cfd
alekseytsvetkov <code.aleksey@icloud.com>
2021-09-24 18:21:25 +0300

    feat(mobile): added splash screen (android/ios)

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -12,7 +12,6 @@ import * as Sentry from '@sentry/react-native';
 import React, { useEffect } from 'react';
 import { LogBox, StatusBar } from 'react-native';
 import Config from 'react-native-config';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
 
 import { LoadingOverlay } from '_app/components';
