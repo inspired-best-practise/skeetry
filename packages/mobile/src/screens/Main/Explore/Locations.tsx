ea6ec59c32455248600d6ffa2e45c5d4e8f4698c
alekseytsvetkov <code.aleksey@icloud.com>
2021-09-25 14:39:35 +0300

    refactor(mobile): updated locations lists

--- packages/mobile/src/screens/Main/Explore/Locations.tsx
+++ packages/mobile/src/screens/Main/Explore/Locations.tsx
@@ -5,7 +5,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { HorizontalCardList } from '_app/components';
-import { OrderDirection, usePopularQuery } from '_app/generated/graphql';
+import { OrderDirection, useCitiesQuery, usePopularQuery } from '_app/generated/graphql';
 
 import { s } from './styles';
 
@@ -13,6 +13,11 @@ export const LocationsScreen = () => {
   const { t } = useTranslation();
   const ref = useRef<ScrollView>(null);
   const [popular, setPopular] = useState();
+  const [europe, setEurope] = useState();
+  const [asia, setAsia] = useState();
+  const [australia, setAustralia] = useState();
+  const [americas, setAmericas] = useState();
+  const [africa, setAfrica] = useState();
 
   const {
     data: dataPopular,
@@ -29,11 +34,121 @@ export const LocationsScreen = () => {
     notifyOnNetworkStatusChange: true,
   });
 
+  const {
+    data: dataEurope,
+    loading: loadingEurope,
+    error: errorEurope,
+    fetchMore: fetchMoreEurope,
+  } = useCitiesQuery({
+    variables: {
+      first: 10,
+      orderBy: {
+        direction: OrderDirection.Asc,
+      },
+      continent: 'EUROPE',
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const {
+    data: dataAsia,
+    loading: loadingAsia,
+    error: errorAsia,
+    fetchMore: fetchMoreAsia,
+  } = useCitiesQuery({
+    variables: {
+      first: 10,
+      orderBy: {
+        direction: OrderDirection.Asc,
+      },
+      continent: 'ASIA',
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const {
+    data: dataAustralia,
+    loading: loadingAustralia,
+    error: errorAustralia,
+    fetchMore: fetchMoreAustralia,
+  } = useCitiesQuery({
+    variables: {
+      first: 10,
+      orderBy: {
+        direction: OrderDirection.Asc,
+      },
+      continent: 'OCEANIA',
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const {
+    data: dataAmericas,
+    loading: loadingAmericas,
+    error: errorAmericas,
+    fetchMore: fetchMoreAmericas,
+  } = useCitiesQuery({
+    variables: {
+      first: 10,
+      orderBy: {
+        direction: OrderDirection.Asc,
+      },
+      continent: 'AMERICAS',
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const {
+    data: dataAfrica,
+    loading: loadingAfrica,
+    error: errorAfrica,
+    fetchMore: fetchMoreAfrica,
+  } = useCitiesQuery({
+    variables: {
+      first: 10,
+      orderBy: {
+        direction: OrderDirection.Asc,
+      },
+      continent: 'AFRICA',
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
   useEffect(() => {
     if (dataPopular) {
       setPopular(dataPopular.popular.edges);
     }
   }, [dataPopular]);
+
+  useEffect(() => {
+    if (dataEurope) {
+      setEurope(dataEurope.cities.edges);
+    }
+  }, [dataEurope]);
+
+  useEffect(() => {
+    if (dataAsia) {
+      setAsia(dataAsia.cities.edges);
+    }
+  }, [dataAsia]);
+
+  useEffect(() => {
+    if (dataAustralia) {
+      setAustralia(dataAustralia.cities.edges);
+    }
+  }, [dataAustralia]);
+
+  useEffect(() => {
+    if (dataAmericas) {
+      setAmericas(dataAmericas.cities.edges);
+    }
+  }, [dataAmericas]);
+
+  useEffect(() => {
+    if (dataAfrica) {
+      setAfrica(dataAfrica.cities.edges);
+    }
+  }, [dataAfrica]);
 
   useScrollToTop(ref);
 
@@ -53,6 +168,91 @@ export const LocationsScreen = () => {
     }
   };
 
+  const europeEndReached = async () => {
+    if (europe) {
+      const lastEurope = europe[europe.length - 1].node.id;
+      const newData = await fetchMoreEurope({
+        variables: {
+          first: 10,
+          after: lastEurope,
+          orderBy: {
+            direction: OrderDirection.Asc,
+          },
+          continent: 'EUROPE',
+        },
+      });
+      setEurope(prevState => [...prevState, ...newData.data.cities.edges]);
+    }
+  };
+
+  const asiaEndReached = async () => {
+    if (asia) {
+      const lastAsia = asia[asia.length - 1].node.id;
+      const newData = await fetchMoreAsia({
+        variables: {
+          first: 10,
+          after: lastAsia,
+          orderBy: {
+            direction: OrderDirection.Asc,
+          },
+          continent: 'ASIA',
+        },
+      });
+      setAsia(prevState => [...prevState, ...newData.data.cities.edges]);
+    }
+  };
+
+  const australiaEndReached = async () => {
+    if (australia) {
+      const lastAustralia = australia[australia.length - 1].node.id;
+      const newData = await fetchMoreAustralia({
+        variables: {
+          first: 10,
+          after: lastAustralia,
+          orderBy: {
+            direction: OrderDirection.Asc,
+          },
+          continent: 'OCEANIA',
+        },
+      });
+      setAustralia(prevState => [...prevState, ...newData.data.cities.edges]);
+    }
+  };
+
+  const americasEndReached = async () => {
+    if (americas) {
+      const lastAmericas = americas[americas.length - 1].node.id;
+      const newData = await fetchMoreAmericas({
+        variables: {
+          first: 10,
+          after: lastAmericas,
+          orderBy: {
+            direction: OrderDirection.Asc,
+          },
+          continent: 'AMERICAS',
+        },
+      });
+      setAmericas(prevState => [...prevState, ...newData.data.cities.edges]);
+    }
+  };
+
+  const africaEndReached = async () => {
+    if (africa) {
+      const lastAfrica = africa[africa.length - 1].node.id;
+      const newData = await fetchMoreAfrica({
+        variables: {
+          first: 10,
+          after: lastAfrica,
+          orderBy: {
+            direction: OrderDirection.Asc,
+          },
+          continent: 'AFRICA',
+        },
+      });
+      setAfrica(prevState => [...prevState, ...newData.data.cities.edges]);
+    }
+  };
+
   return (
     <SafeAreaView style={s.container}>
       <ScrollView ref={ref} showsVerticalScrollIndicator={false} scrollsToTop={true}>
@@ -65,7 +265,7 @@ export const LocationsScreen = () => {
             loading={loadingPopular}
           />
         )}
-        {!errorPopular && (
+        {/* {!errorPopular && (
           <HorizontalCardList
             title={`${t('explore:forYou')}`}
             data={popular}
@@ -73,68 +273,50 @@ export const LocationsScreen = () => {
             handleEndReached={popularEndReached}
             loading={loadingPopular}
           />
-        )}
-        {!errorPopular && (
+        )} */}
+        {!errorEurope && (
           <HorizontalCardList
             title={`${t('explore:europe')}`}
-            data={popular}
+            data={europe}
             size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
+            handleEndReached={europeEndReached}
+            loading={loadingEurope}
           />
         )}
-        {!errorPopular && (
+        {!errorAsia && (
           <HorizontalCardList
             title={`${t('explore:asia')}`}
-            data={popular}
+            data={asia}
             size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
+            handleEndReached={asiaEndReached}
+            loading={loadingAsia}
           />
         )}
-        {!errorPopular && (
+        {!errorAustralia && (
           <HorizontalCardList
             title={`${t('explore:australia')}`}
-            data={popular}
+            data={australia}
             size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
+            handleEndReached={australiaEndReached}
+            loading={loadingAustralia}
           />
         )}
-        {!errorPopular && (
+        {!errorAmericas && (
           <HorizontalCardList
-            title={`${t('explore:antarctica')}`}
-            data={popular}
+            title={`${t('explore:americas')}`}
+            data={americas}
             size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
+            handleEndReached={americasEndReached}
+            loading={loadingAmericas}
           />
         )}
-        {!errorPopular && (
-          <HorizontalCardList
-            title={`${t('explore:north_america')}`}
-            data={popular}
-            size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
-          />
-        )}
-        {!errorPopular && (
-          <HorizontalCardList
-            title={`${t('explore:south_america')}`}
-            data={popular}
-            size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
-          />
-        )}
-        {!errorPopular && (
+        {!errorAfrica && (
           <HorizontalCardList
             title={`${t('explore:africa')}`}
-            data={popular}
+            data={africa}
             size="small"
-            handleEndReached={popularEndReached}
-            loading={loadingPopular}
+            handleEndReached={africaEndReached}
+            loading={loadingAfrica}
           />
         )}
       </ScrollView>
