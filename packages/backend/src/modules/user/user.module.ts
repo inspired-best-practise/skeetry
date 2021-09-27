44c6b010f3be397512d0ef3c0efa672b59f5c5a9
alekseytsvetkov <code.aleksey@icloud.com>
2021-09-27 19:38:48 +0300

    fix(backend): updated user module

--- packages/backend/src/modules/user/user.module.ts
+++ packages/backend/src/modules/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule, HttpModule],
   controllers: [],
-  providers: [UserResolver],
+  providers: [UserResolver, UserService],
   exports: [],
 })
 export class UserModule {}
