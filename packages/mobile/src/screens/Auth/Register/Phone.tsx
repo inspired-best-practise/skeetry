ae9ac5ca3cd10318d9b6f6213ccd58999114f8cc
alekseytsvetkov <code.aleksey@icloud.com>
2021-11-05 16:31:52 +0300

    refactor(mobile): updated phone input

--- packages/mobile/src/screens/Auth/Register/Phone.tsx
+++ packages/mobile/src/screens/Auth/Register/Phone.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, useColorScheme } from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { FormWrapper } from '_app/components';
-import { PLATFORM } from '_app/constants';
+import { colors, radius } from '_app/constants';
 import { useSendSmsCodeMutation } from '_app/generated/graphql';
 import { navigation } from '_app/services/navigations';
 import { regStore } from '_app/stores';
@@ -14,6 +15,12 @@ import { s } from './styles';
 
 export const PhoneScreen = () => {
   const { t } = useTranslation();
+  const theme = useColorScheme();
+
+  const [formattedValue, setFormattedValue] = useState('');
+  const [valid, setValid] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const phoneInput = useRef<PhoneInput>(null);
 
   const [sendSmsCodeMutation, { data, loading, error }] = useSendSmsCodeMutation();
 
@@ -24,18 +31,33 @@ export const PhoneScreen = () => {
     handleSubmit,
     formState: { errors },
     getValues,
+    setError,
   } = useForm();
-  const onSubmit = ({ phone }) => {
-    sendSmsCodeMutation({
-      variables: {
-        phone,
-      },
-    });
+  const onSubmit = () => {
+    const formattedPhone = formattedValue.replace('+', '');
+
+    if (valid) {
+      sendSmsCodeMutation({
+        variables: {
+          phone: formattedPhone,
+        },
+      });
+    } else {
+      setError('phone', { message: `${t('utils:wrong_phone')}` });
+    }
   };
+
+  const phone = getValues().phone;
+
+  useEffect(() => {
+    const checkValid = phoneInput.current?.isValidNumber(phone);
+    setShowMessage(true);
+    setValid(checkValid ? checkValid : false);
+  }, [phone]);
 
   useEffect(() => {
     if (data && data.sendSmsCode === true) {
-      setPhone(getValues().phone);
+      setPhone(phone);
       return navigation.push('Code');
     }
   }, [data]);
@@ -49,25 +71,36 @@ export const PhoneScreen = () => {
           control={control}
           rules={{ required: true, minLength: 6, maxLength: 12 }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <View style={s.textInputWrapper}>
-              <TextInput
-                style={s.input}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                autoCapitalize="none"
-                placeholder={t('utils:phone')}
-                spellCheck={false}
-                keyboardType={PLATFORM.IS_ANDROID ? 'numeric' : 'number-pad'}
-              />
-            </View>
+            <PhoneInput
+              ref={phoneInput}
+              placeholder={t('utils:phone')}
+              filterProps={{ placeholder: t('utils:enter_country_name') }}
+              countryPickerProps={{
+                translation: t('utils:picker_lang'),
+              }}
+              withDarkTheme={theme === 'dark' ? true : false}
+              defaultValue={value}
+              defaultCode="RU"
+              layout="first"
+              onChangeText={onChange}
+              textInputProps={{
+                onBlur,
+              }}
+              onChangeFormattedText={text => {
+                setFormattedValue(text);
+              }}
+              autoFocus
+              // disableArrowIcon
+              containerStyle={{ width: '100%', backgroundColor: colors.gray100, borderRadius: radius.base }}
+              textContainerStyle={{ backgroundColor: colors.gray100, borderRadius: radius.base }}
+            />
           )}
           name="phone"
           defaultValue=""
         />
         {errors.phone && (
           <Text style={s.errorLogin}>
-            {t('utils:phone')} {t('utils:is_required')}
+            {errors.phone.message ? errors.phone.message : `${t('utils:phone')} ${t('utils:is_required')}`}
           </Text>
         )}
         <TouchableOpacity
